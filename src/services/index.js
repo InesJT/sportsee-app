3e6b13540b3e5225ef058e9ab7df68b4c598d19a
Ines Trabelsi <ines.jenfaoui@gmail.com>
2025-02-14 17:13:05 +0100

    Feat: Add service to fetch user activity

--- src/services/index.js
+++ src/services/index.js
@@ -2,15 +2,25 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-const fetchUserInfo = async (id) => {
-   try {
-    const response = await axios.get(`${BASE_URL}/user/${id}`);
+const fetchUserInfo = async (userId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/user/${userId}`);
     return response.data.data.userInfos;
   } catch (error) {
     console.log('error occurred while fetching User info ', error);
   }
 };
 
+const fetchActivity = async (userId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/user/${userId}/activity`);
+    return response.data.data.sessions;
+  } catch (error) {
+    console.log('error occurred while fetching User activities ', error);
+  }
+}
+
 export {
   fetchUserInfo,
+  fetchActivity,
 };
